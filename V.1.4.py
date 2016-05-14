15dd650ea8d391f2f28bce4f3a7f5945834de7d8
Aitlade <titouan@rannou.pl>
2016-05-15 00:06:18 +0200

    derniere version après l'explosion
    tout fonctionne nickel plus qu'a savoir comment gérer le temps

--- V.1.4.py
+++ V.1.4.py
@@ -14,17 +14,17 @@ from random import*
 
 fen=Tk()
 fen.title("Menu")
-can=Canvas(fen, width =550, height =550, bg ="black")
+can=Canvas(fen, width =550, height =550, bg ="lime")
 can.pack(side=TOP,padx=5,pady=5)
 x,y=0,0
 can.create_rectangle(200,100,x+350,y+150,fill="red",tags="menu")
 can.create_rectangle(200,170,x+350,y+220,fill="red",tags="menu")
 can.create_rectangle(200,240,x+350,y+290,fill="red",tags="menu")
 can.create_rectangle(200,310,x+350,y+360,fill="red",tags="menu")
-can.create_text(275,125,text="Nouvelle partie")
-can.create_text(275,195,text="Scores")
-can.create_text(275,265,text="Règles")
-can.create_text(275,335,text="Quitter")
+can.create_text(275,125,text="Nouvelle partie",tags="menu")
+can.create_text(275,195,text="Scores",tags="menu")
+can.create_text(275,265,text="Règles",tags="menu")
+can.create_text(275,335,text="Quitter",tags="menu")
 
 def menu(event):
     global x,y,jouer
@@ -107,49 +107,7 @@ Bombe2=PhotoImage(file="bombebleue.png")
 range_bombe1=0
 range_bombe2=0
 
-def explosion(xj,yj,joueur):
-    global Victoire, range_bombe1,range_bombe2
-    print(can.find_overlapping(xj,yj,xj+50,yj+50))
-    destroy=[]
-    a1=can.find_overlapping(xj-1-range_bombe1*50,yj,xj+51+range_bombe1*50,yj+50)
-    a2=can.find_overlapping(xj,yj-1-range_bombe1*50,xj+50,yj+51+range_bombe1*50)
-    b=can.find_withtag('briques')
-    for i in range (len(a1)):
-        for j in range (len(b)):
-            if  a1[i]==b[j]:
-                xb=can.bbox(b[j])[0]
-                yb=can.bbox(b[j])[1]
-                bonus_bombe(xb,yb)
-                destroy.append(b[j])
-                
-            if a1[i]==1 and joueur==2:
-                Victoire=2              
-            if a1[i]==2 and joueur==1:
-                Victoire=1
-    for i in range (len(a2)):
-        for j in range(len(b)):
-            if a2[i]==b[j]:
-                xb=can.bbox(b[j])[0]
-                yb=can.bbox(b[j])[1]
-                bonus_bombe(xb,yb)
-                destroy.append(b[j])
-                 
-            if a2[i]==1 and joueur==2:
-                Victoire=2
-            if a2[i]==2 and joueur==1:
-                Victoire=1               
-    for i in range(len(destroy)):
-        can.delete(destroy[i])
-    if joueur==1:
-        can.delete('Bombe1')
-    if joueur==2:
-        can.delete('Bombe2')
-    if Victoire==1:
-        print('Le joueur 1 a gagné')
-        can.delete('Perso2')
-    if Victoire==2:
-        print('Le joueur 2 a gagné')
-        can.delete('Perso')
+
 
 def bombe1(event):
     # en entrée : event
@@ -168,6 +126,80 @@ def bombe2(event):
     a=xj2
     b=yj2
     explosion(a,b,2)
+
+def explosion(x,y,joueur):
+    global Victoire, range_bombe1,range_bombe2
+    destroy=[]
+    if joueur==1:
+        bonus=range_bombe1
+    elif joueur==2:
+        bonus=range_bombe2
+    a=0
+    explosion_bas=False
+    explosion_droite=False
+    explosion_haut=False
+    explosion_gauche=False
+    while explosion_bas==False and a<=bonus:
+        for i in range(len(can.find_overlapping(x,y+50,x+50,y+51+a*50))):
+            for j in range(len(can.find_withtag('briques'))):
+                if can.find_overlapping(x,y+50,x+50,y+51+a*50)[i]==can.find_withtag('briques')[j]:
+                    explosion_bas=True
+                    xb=can.bbox(can.find_withtag('briques')[j])[0]
+                    yb=can.bbox(can.find_withtag('briques')[j])[1]
+                    bonus_bombe(xb,yb)
+                    destroy.append(can.find_withtag('briques')[j])
+            for k in range(len(can.find_withtag('blocs'))):
+                if can.find_overlapping(x,y+50,x+50,y+51+a*50)[i]==can.find_withtag('blocs')[k]:
+                    explosion_bas=True
+        a+=1
+    a=0
+    while explosion_droite==False and a<=bonus:      
+        for i in range(len(can.find_overlapping(x+50,y,x+51+50*a,y+50))):
+            for j in range(len(can.find_withtag('briques'))):
+                if can.find_overlapping(x+50,y,x+51+50*a,y+50)[i]==can.find_withtag('briques')[j]:
+                    explosion_droite=True
+                    xb=can.bbox(can.find_withtag('briques')[j])[0]
+                    yb=can.bbox(can.find_withtag('briques')[j])[1]
+                    bonus_bombe(xb,yb)
+                    destroy.append(can.find_withtag('briques')[j])
+            for k in range(len(can.find_withtag('blocs'))):
+                if can.find_overlapping(x+50,y,x+51+50*a,y+50)[i]==can.find_withtag('blocs')[k]:
+                    explosion_droite=True
+        a+=1
+    a=0
+    while explosion_haut==False and a<=bonus:
+        for i in range(len(can.find_overlapping(x,y,x+50,y-1-a*50))):
+            for j in range(len(can.find_withtag('briques'))):
+                if can.find_overlapping(x,y,x+50,y-1-a*50)[i]==can.find_withtag('briques')[j]:
+                    explosion_haut=True
+                    xb=can.bbox(can.find_withtag('briques')[j])[0]
+                    yb=can.bbox(can.find_withtag('briques')[j])[1]
+                    bonus_bombe(xb,yb)
+                    destroy.append(can.find_withtag('briques')[j])
+            for k in range(len(can.find_withtag('blocs'))):
+                if can.find_overlapping(x,y,x+50,y-1-a*50)[i]==can.find_withtag('blocs')[k]:
+                    explosion_haut=True
+        a+=1
+    a=0
+    while explosion_gauche==False and a<=bonus:
+        for i in range(len(can.find_overlapping(x-1-a*50,y,x,y+50))):
+            for j in range(len(can.find_withtag('briques'))):
+                if can.find_overlapping(x-1-a*50,y,x,y+50)[i]==can.find_withtag('briques')[j]:
+                    explosion_gauche=True
+                    xb=can.bbox(can.find_withtag('briques')[j])[0]
+                    yb=can.bbox(can.find_withtag('briques')[j])[1]
+                    bonus_bombe(xb,yb)
+                    destroy.append(can.find_withtag('briques')[j])
+            for k in range(len(can.find_withtag('blocs'))):
+                if can.find_overlapping(x-1-a*50,y,x,y+50)[i]==can.find_withtag('blocs')[k]:
+                    explosion_gauche=True
+        a+=1
+    for i in range(len(destroy)):
+        can.delete(destroy[i])
+    if joueur==1:
+        can.delete('Bombe1')
+    if joueur==2:
+        can.delete('Bombe2')
 ### BONUS ###
 
 Bombe_bonus=PhotoImage(file="bonus_bombe.png")
@@ -189,8 +221,54 @@ def verif_bonus_bombe():
             if  a[i]==b[j]:
                 destroy.append(b[j])
                 range_bombe1+=1
-    can.delete(destroy[0])
+    if len(destroy)==1:
+        can.delete(destroy[0])
+
+def verif_bonus_bombe1():
+    global xj2,yj2,range_bombe1,range_bombe2
+    destroy=[]
+    a=can.find_enclosed(xj2,yj2,xj2+50,yj2+50)
+    b=can.find_withtag('bb')
+    for i in range (len(a)):
+        for j in range (len(b)):
+            if  a[i]==b[j]:
+                destroy.append(b[j])
+                range_bombe2+=1
+    if len(destroy)==1:
+        can.delete(destroy[0])
+   
+trap1_bonus=PhotoImage(file="trap1.png")
+def trap1(event):
+    can.create_image(xj,yj,image=trap1_bonus,anchor=NW,tags="tb1")
+
+trap2_bonus=PhotoImage(file="trap2.png")
+def trap2(event):
+    can.create_image(xj2,yj2,image=trap2_bonus,anchor=NW,tags="tb2")
+
+def verif_trap1():
+    global xj2,yj2
+    destroy=[]
+    a=can.find_enclosed(xj2,yj2,xj2+50,yj2+50)
+    b=can.find_withtag('tb1')
+    for i in range (len(a)):
+        for j in range (len(b)):
+            if  a[i]==b[j]:
+                destroy.append(b[j])
+    if len(destroy)==1:
+        can.delete(destroy[0])
     
+def verif_trap2():
+    global xj,yj
+    destroy=[]
+    a=can.find_enclosed(xj,yj,xj+50,yj+50)
+    b=can.find_withtag('tb2')
+    for i in range (len(a)):
+        for j in range (len(b)):
+            if  a[i]==b[j]:
+                destroy.append(b[j])
+    if len(destroy)==1:
+        can.delete(destroy[0])
+
                 
 ### Personnages / joueurs ###
 
@@ -228,6 +306,7 @@ def animdroite(event): # déplace le joueur vers la droite
         xj+=50
         can.coords("perso",xj,yj)
         verif_bonus_bombe()
+        verif_trap2()
 
 def animgauche(event): # déplace le joueur1 vers la gauche
     global xj,yj
@@ -243,6 +322,7 @@ def animgauche(event): # déplace le joueur1 vers la gauche
         xj-=50
         can.coords("perso",xj,yj)
         verif_bonus_bombe()
+        verif_trap2()
 
 def animbas(event): # déplace le joueur1 vers le bas
     global xj,yj
@@ -258,6 +338,7 @@ def animbas(event): # déplace le joueur1 vers le bas
         yj+=50
         can.coords("perso",xj,yj)
         verif_bonus_bombe()
+        verif_trap2()
 
 def animhaut(event): # déplace le joueur1 vers le haut
     global xj,yj
@@ -273,6 +354,7 @@ def animhaut(event): # déplace le joueur1 vers le haut
         yj-=50
         can.coords("perso",xj,yj)
         verif_bonus_bombe()
+        verif_trap2()
 
     ### Joueur 2 ###
 
@@ -291,6 +373,8 @@ def animdroite2(event): # déplace le joueur2 vers la droite
     if a==True:
         xj2+=50
         can.coords("perso2",xj2,yj2)
+        verif_bonus_bombe1()
+        verif_trap1()
 
 def animgauche2(event): # déplace le joueur2 vers la gauche
     global xj2,yj2
@@ -305,6 +389,8 @@ def animgauche2(event): # déplace le joueur2 vers la gauche
     if a==True:
         xj2-=50
         can.coords("perso2",xj2,yj2)
+        verif_bonus_bombe1()
+        verif_trap1()
 
 def animbas2(event): # déplace le joueur2 vers le bas
     global xj2,yj2
@@ -319,6 +405,8 @@ def animbas2(event): # déplace le joueur2 vers le bas
     if a==True:
         yj2+=50
         can.coords("perso2",xj2,yj2)
+        verif_bonus_bombe1()
+        verif_trap1()
 
 def animhaut2(event): # déplace le joueur2 vers le haut
     global xj2,yj2,place2
@@ -333,6 +421,8 @@ def animhaut2(event): # déplace le joueur2 vers le haut
     if a==True:
         yj2-=50
         can.coords("perso2",xj2,yj2)
+        verif_bonus_bombe1()
+        verif_trap1()
 	
 ### Programme ###
 
@@ -352,6 +442,7 @@ fen.bind("<KeyRelease-Right>",animdroite)
 fen.bind("<KeyRelease-Up>",animhaut)
 fen.bind("<KeyRelease-Down>",animbas)
 fen.bind("<Key-Return>",bombe1)
+fen.bind("<KeyRelease-a>",trap2)
 
 ## Commandes joueur 2 ##
 fen.bind("<KeyRelease-q>",animgauche2)
@@ -359,7 +450,7 @@ fen.bind("<KeyRelease-d>",animdroite2)
 fen.bind("<KeyRelease-z>",animhaut2)
 fen.bind("<KeyRelease-s>",animbas2)
 fen.bind("<space>",bombe2)
-
+fen.bind("<KeyRelease-0>",trap1)
 
 fen.mainloop()
 fen.destroy()
